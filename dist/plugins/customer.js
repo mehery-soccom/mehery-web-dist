7d7ca0027c4a8faeb7b3ef1d935697fffe24c0a0
lnt <lalit.tanwar07@gmail.com>
2021-06-26 23:37:35 +0000

    Deploying to gh-pages from @ mehery-soccom/mehery-web@35d01dea345e072f48e31e9008967a6daec701d3 🚀

---
+++ dist/plugins/customer.js
@@ -0,0 +1,151 @@
+(function (win) {
+
+    var maxHeight = 500;
+    var minHeight = 60;
+    var maxWidth = 400;
+    var minWidth = 60;
+    var UNIQUEID = '3ix5ju9kqbtr2W';
+
+    var myChat = {
+        init : function (options) {
+            options = Object.assign({
+                entity : "api", // After which chat is expired
+            },options);
+
+            options.path = ["https://",options.entity,".mehery.com/postman/plugin/customer/app/chat/"].join("");
+            //options.path = ["http://local-kwt.amxremit.com:8082/postman/plugin/customer/app/chat/"].join("");
+            //local-kwt.amxremit.com:8083/agent/app/home
+            options.query = ["CDN_URL=http://127.0.0.1:8080"]
+            console.log("init",options);
+            var div = document.createElement("div");
+            document.getElementsByTagName('body')[0].appendChild(div);
+            div.outerHTML = `
+            <div id='myChatDiv' class="myChatDivClose">
+                <div id='botTitleBar' 
+                     style='height: ${minHeight}px; width: ${minWidth}px; position:fixed; cursor: pointer;'></div>
+                <div id='myChatFrame' style='margin: 0px 0px 0px 0px;'>
+                    <iframe id="myChatIFrame-${UNIQUEID}"
+                            src=${options.path}?${options.query}></iframe>
+                <div>
+            </div>
+            <style>
+                #myChatDiv {
+                    height: ${minHeight+2}px; width: ${minWidth+2}px; 
+                    position: fixed; bottom: 10px; right:10px; z-index: 1000; 
+                    background-color: transparent; 
+                    overflow:hidden;
+                    border-radius: 150px!important;
+                    padding:0px!important; 
+                    margin:0px!important
+                }
+                #myChatDiv #myChatFrame {
+                    maring: 0px 0px 0px 0px; 
+                }
+                #myChatDiv #myChatFrame #myChatIFrame-${UNIQUEID} {
+                    border-width: 0px; 
+                    margin: 1px 1px 1px 1px;
+                    height: ${minHeight}px; width: ${minWidth}px; 
+                }
+                .myChatDivOpen#myChatDiv {
+                    border-radius: 10px!important;
+                    height: ${maxHeight}px; width: ${maxWidth}px; 
+                    height: calc(100vh - 55px);
+                    max-height: ${maxHeight}px;
+                }
+                .myChatDivOpen#myChatDiv #myChatFrame {
+                    maring: 0px 0px 0px 0px; 
+                }
+                .myChatDivOpen#myChatDiv #myChatFrame #myChatIFrame-${UNIQUEID} {
+                    margin: 0px 0px 0px 0px;
+                    height: ${maxHeight}px; width: ${maxWidth}px; 
+                    height: calc(100vh - 55px);
+                    max-height: ${maxHeight}px;
+                }
+                @media (max-width: 450px) {
+                    .myChatDivOpen#myChatDiv,
+                    .myChatDivOpen#myChatDiv #myChatFrame #myChatIFrame-${UNIQUEID} {
+                        width: 100vw;
+                        height: 100vh;
+                        max-height: 100vh;
+                        right: 0;
+                        bottom: 0;
+                        border-radius: 0px!important;
+                        transition: .1s ease-in-out;
+                    }
+                }
+
+            </style>
+            `;
+            this.$myChatIFrame = document.getElementById(`myChatIFrame-${UNIQUEID}`);
+            this.$myChatDiv = document.querySelector('#myChatDiv');
+            this.$myChatFrame = document.querySelector('#myChatFrame');
+
+            var THAT = this;
+            document.querySelector('body').addEventListener('click', function (e) {
+                e.target.matches = e.target.matches || e.target.msMatchesSelector;
+                if (e.target.matches('#botTitleBar')) { 
+                    THAT.postMessage( { event : "CHAT_TOGGLE" });
+                };
+            });
+
+
+
+        },
+        chatOnClose : function (argument) {
+            this.$myChatDiv.className = this.$myChatDiv.className.replace("myChatDivOpen",'myChatDivClose');
+        },
+        chatOnOpen : function (argument) {
+            this.$myChatDiv.className = this.$myChatDiv.className.replace("myChatDivClose",'myChatDivOpen');
+        },
+        postMessage : function (obj) {
+            var msg = JSON.stringify({myChatEvent : obj});
+            console.log("PARENT:sendPostMessage",msg);
+            this.$myChatIFrame.contentWindow.postMessage(msg, '*');
+        },
+        bindEvent : function(element, eventName, eventHandler) {
+            if (element.addEventListener){
+                element.addEventListener(eventName, eventHandler, false);
+            } else if (element.attachEvent) {
+                element.attachEvent('on' + eventName, eventHandler);
+            }
+        }
+    };
+
+    myChat.bindEvent(win,'message',function (e) {
+        console.log("PARENT:onPostMessage",e);
+        if(!e || !e.data || typeof e.data != 'string' || e.data.trim().indexOf("{")!=0){
+            return;
+        }
+        var data = JSON.parse(e.data);
+        if(!data.myChatEvent){
+            return;
+        }
+        var myChatEvent = data.myChatEvent;
+        if(myChatEvent.event == "ON_CHAT_TOGGLE"){
+            if(myChatEvent.isChatOpen) myChat.chatOnOpen(); else myChat.chatOnClose();
+        }
+    });
+
+    var scripts = document.getElementsByTagName('script');    
+    var index = scripts.length - 1;
+    var myScript = scripts[index];
+    var serviceUrl = myScript.src; 
+    try {
+        return myChat.init(JSON.parse(myScript.innerHTML));
+    } catch(e){
+        console.error(e);
+    }
+    var pluginUrl = "https://api.mehery.com/postman/plugin/customer/app/chat/";
+    if(serviceUrl.indexOf("/assets/js/customer.plugin.")>0){
+    	var urlparts = serviceUrl.split("/assets/js/customer.plugin.");
+    	var domainUrl = urlparts[0];
+    	var theme = (urlparts[1].split("theme=")[1] || "").split("&")[0];
+    	if(theme){
+    		pluginUrl = domainUrl + "/" + "dummy/customer?page=plugin."+theme;    		
+    	}
+    }
+    console.log("pluginUrl",pluginUrl)
+   
+
+    win.myChat = myChat;
+}(this));
